ef7e351a6edc5cb898b331fc5a9b9ad274686ae9
Rituja Shinde <65076102+ritujashinde97@users.noreply.github.com>
2020-11-24 11:07:37 +0530

    Revert "Rating:800"
    This reverts commit 0abd1faf

--- A/116A_Tram.py
+++
@@ -1,9 +0,0 @@
-s = int(input())
-no_of_pass = 0
-capacity = 0
-for i in range(s):
-    ex, en = map(int, input().split())
-    no_of_pass -= ex
-    no_of_pass += en
-    capacity = max(capacity, no_of_pass)
-print(capacity)
